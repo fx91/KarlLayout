bde974970c99e034941feb000757c6ee970e1308
fx <fangxiao@zdworks.com>
2016-08-18 01:07:11 +0800

    Initial Commit

---
+++ KarlLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KarlLayout
+//
+//  Created by fx on 16/8/18.
+//  Copyright © 2016年 fx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KarlLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KarlLayout
+//
+//  Created by fx on 16/8/18.
+//  Copyright © 2016年 fx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
